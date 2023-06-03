daf139aee500bafc152e6a79aef3a6198c65e92e
zonelder <45139960+zonelder@users.noreply.github.com>
2023-06-03 17:12:26 +0500

    changes to support vector algebra notation

--- complex.lua
+++ complex.lua
@@ -1,14 +1,19 @@
----@class Complex # Complex number class
----@field real number # real part of a complex number
----@field imagin number # imagin part of a complex number
-local Complex = {}
+---@class Complex : number[2] # Complex number class
+---@field sqr_len fun(self: Complex| number) # Calculate squared module of the complex number. return  an error if input cannot be convert into complex number
+---@field len fun(self: Complex| number) # Calculate module of the complex number
+---@field conjugate fun(self: Complex | number) # calculate conjugation of complex number
+---@field Re fun(self: Complex|number) # return real part of the complex number
+---@field Im fun(self: Complex|number) # return imagin part of the complex number
+local ComplexNumber = {}
+
+local Complex_class = {}
 
 ---@return Complex?
 local function convert_to_complex(a)
     if type(a) == "table" then
-        return Complex:new(a.real,a.imagin)
+        return Complex_class.new(a[1],a[2])
     elseif type(a) == "number" then
-        return Complex:new(a,0)
+        return Complex_class.new(a,0)
     end
 end
 
@@ -16,116 +21,143 @@ end
 ---@param real number?
 ---@param imagin number?
 ---@return Complex
-function Complex.new(self,real,imagin)
-    local num = setmetatable({},self)
-    num.real = real or 0
-    num.imagin = imagin or 0
+function Complex_class.new(real,imagin)
+    local num = setmetatable({},ComplexNumber)
+    num[1] = real or 0
+    num[2] = imagin or 0
     return num
 end
 
 --- calculate squared module of the complex number. return  an error if input cannot be convert into complex number
----@param self Complex | number
+---@param self Complex | number[]
 ---@return number?
 ---@return string? #error log
-function Complex.sqr_len(self)
+function Complex_class.sqr_len(self)
     local self = convert_to_complex(self)
     if not self then return nil,"agument is not a number" end
 
-    return self.real^2 + self.imagin^2
+    return self[1]^2 + self[2]^2
 end
 
 ---calculate conjugation of complex number
----@param self  Complex | number
+---@param self  Complex | number[]
 ---@return Complex?
 ---@return string? #error log
-function Complex.conjugate(self)
+function Complex_class.conjugate(self)
     local self = convert_to_complex(self)
     if not self then return nil,"agument is not a number" end
 
-    return Complex:new(self.real,-self.imagin)
+    return Complex_class.new(self[1],-self[2])
 end
 
 ---calculate module of the complex number
----@param self Complex | number
+---@param self Complex | number[]
 ---@return number?
 ---@return string? #error log
-function Complex.len(self)
+function Complex_class.len(self)
     local self = convert_to_complex(self)
     if not self then return nil,"agument is not a number" end
 
-    return  math.sqrt(Complex.sqr_len(self) or 1)
+    return  math.sqrt(Complex_class.sqr_len(self) or 1)
+end
+
+
+--- return real part of the number
+---@param self Complex | number[]
+---@return number?
+---@return string? #error log
+function Complex_class.Re(self)
+    local self = convert_to_complex(self)
+    if not self then return nil,"agument is not a number" end
+    return self[1]
+end
+
+--- return imagin part of the number
+---@param self Complex | number[]
+---@return number?
+---@return string? #error log
+function Complex_class.Im(self)
+    local self = convert_to_complex(self)
+    if not self then return nil,"agument is not a number" end
+    return self[2]
 end
 
 
 
 --- multiply two complex numbers
----@param a Complex | number
----@param b Complex | number
+---@param a Complex | number[]
+---@param b Complex | number[]
 ---@return Complex?
 ---@return string? #error log
-function Complex.__mul(a,b)
+function ComplexNumber.__mul(a,b)
     local a = convert_to_complex(a)
     local b = convert_to_complex(b)
     if not a or not b then return nil,"agument is not a number" end
 
-    return Complex:new(a.real*b.real - a.imagin*b.imagin , a.real*b.imagin + a.imagin*b.real)
+    return Complex_class.new(a[1]*b[1] - a[2]*b[2] , a[1]*b[2] + a[2]*b[1])
 end
 
 --- summ two complex numbers
----@param a Complex | number
----@param b Complex | number
+---@param a Complex | number[]
+---@param b Complex | number[]
 ---@return Complex?
 ---@return string? #error log
-function Complex.__add(a,b)
+function ComplexNumber.__add(a,b)
     local a = convert_to_complex(a)
     local b = convert_to_complex(b)
     if not a or not b then return nil,"agument is not a number" end
 
-    return Complex:new(a.real + b.real , a.imagin + b.imagin)
+    return Complex_class.new(a[1] + b[1] , a[2] + b[2])
 end
 
 --- subtract first complex number from second one
----@param a Complex | number
----@param b Complex | number
+---@param a Complex | number[]
+---@param b Complex | number[]
 ---@return Complex?
 ---@return string? #error log
-function Complex.__sub(a,b)
+function ComplexNumber.__sub(a,b)
     local a = convert_to_complex(a)
     local b = convert_to_complex(b)
     if not a or not b then return nil,"agument is not a number" end
     
-    return Complex:new(a.real - b.real,a.imagin - b.imagin)
+    return Complex_class.new(a[1] - b[1],a[2] - b[2])
 end
 
 
----@param a Complex | number
+---@param a Complex | number[]
 ---@return Complex?
 ---@return string? #error log
-function Complex.__unm(a)
+function ComplexNumber.__unm(a)
     local a = convert_to_complex(a)
     if not a then return nil,"agument is not a number" end
-    return Complex:new(-a.real,-a.imagin)
+    return Complex_class.new(-a[1],-a[2])
 end
 
 --- divide first complex number from second one
----@param a Complex | number
----@param b Complex | number
+---@param a Complex | number[]
+---@param b Complex | number[]
 ---@return Complex?
 ---@return string? #error log
-function Complex.__div(a,b)
+function ComplexNumber.__div(a,b)
     local a = convert_to_complex(a)
     local b = convert_to_complex(b)
     if not a or not b then return nil,"agument is not a number" end
 
-    local b_sqr_len = Complex.sqr_len(b)
-    local real = a.real*b.real + a.imagin*b.imagin
-    local imagin = a.imagin*b.real- a.real*b.imagin 
-    return  Complex:new(real/b_sqr_len,imagin/b_sqr_len)
+    local b_sqr_len = Complex_class.sqr_len(b)
+    local real = a[1]*b[1] + a[2]*b[2]
+    local imagin = a[2]*b[1]- a[1]*b[2] 
+    return  Complex_class.new(real/b_sqr_len,imagin/b_sqr_len)
 end
 
 
-function Complex.__tostring(self)
-    return string.format("( %s , %s )",self.real,self.imagin)
+function ComplexNumber.__tostring(self)
+    return string.format("( %s , %s )",self[1],self[2])
 end
 
-return Complex
+function ComplexNumber.__index(self,key)
+    return Complex_class[key]
+end
+
+return setmetatable(Complex_class,{__call = function (self,a,b)
+    return Complex_class.new(a,b)
+end})
